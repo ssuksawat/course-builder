0cccdf41f2fc0a4a1685cb8c6401ed821b0f63b1
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-09-25 18:23:52 -0700

    add redux devtools

--- client/config/middleware.js
+++ client/config/middleware.js
@@ -1,10 +1,12 @@
+import { applyMiddleware, compose } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
-const middleware = [thunk, promiseMiddleware()];
+const devTools = window.devToolsExtension ? window.devToolsExtension() : f => f;
+const middlewares = [thunk, promiseMiddleware()];
 if (process.env.NODE_ENV === 'development') {
-  middleware.push(logger());
+  middlewares.push(logger());
 }
 
-export default middleware;
+export default compose(applyMiddleware(...middlewares), devTools);
--- client/index.jsx
+++ client/index.jsx
@@ -2,14 +2,14 @@ import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 
 import './config/vendor';
 import routes from './config/routes';
 import reducer from './config/reducer';
 import middleware from './config/middleware';
 
-const store = createStore(reducer, applyMiddleware(...middleware));
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
   <Provider store={store}>
